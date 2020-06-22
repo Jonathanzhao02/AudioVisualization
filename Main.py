69d0b9f22aaf81fca69c05d1dd3c0dc8b3e58edc
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2020-06-22 13:56:04 -0700

    added comments and fixed indentations to silence pylint's annoying remarks

--- Main.py
+++ Main.py
@@ -1,15 +1,26 @@
+"""
+Sets up and executes an instance of an AudioVisualizer object using computer audio
+"""
+
 import pyaudio
 
-from AudioVisualizer import AudioVisualizer
+from audio_visualizer import AudioVisualizer
 
-PyAudio = pyaudio.PyAudio()
-devices = [PyAudio.get_device_info_by_index(i) for i in range(PyAudio.get_device_count())]
+py_audio = pyaudio.PyAudio()
+devices = [py_audio.get_device_info_by_index(i) for i in range(py_audio.get_device_count())]
 
 FORMAT = pyaudio.paInt16
-CHANNELS = min(PyAudio.get_default_input_device_info()['maxInputChannels'], PyAudio.get_default_output_device_info()['maxOutputChannels'], 1)
-RATE = int(min(PyAudio.get_default_input_device_info()['defaultSampleRate'], PyAudio.get_default_output_device_info()['defaultSampleRate']))
+CHANNELS = min(py_audio.get_default_input_device_info()['maxInputChannels'],
+               py_audio.get_default_output_device_info()['maxOutputChannels'])
+RATE = int(min(py_audio.get_default_input_device_info()['defaultSampleRate'],
+               py_audio.get_default_output_device_info()['defaultSampleRate']))
 FRAMES_PER_BUFFER = 1024
 LOG_MODE = False
 
-visualizer = AudioVisualizer(PyAudio=PyAudio, format=FORMAT, channels=CHANNELS, sample_rate=RATE, chunk_size=FRAMES_PER_BUFFER, log_mode=LOG_MODE)
+visualizer = AudioVisualizer(py_audio=py_audio,
+                             data_format=FORMAT,
+                             channels=CHANNELS,
+                             sample_rate=RATE,
+                             chunk_size=FRAMES_PER_BUFFER,
+                             log_mode=LOG_MODE)
 visualizer.start()
--- audio_visualizer.py
+++ audio_visualizer.py
@@ -1,11 +1,14 @@
-import numpy as np
-import math
+"""
+Contains the AudioVisualizer object
+"""
+
 import sys
-import wave
-import pyaudio
 
 from queue import Queue
 from threading import Thread, Event
+
+import pyaudio
+import numpy as np
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
@@ -15,11 +18,17 @@ from scipy.signal.windows import blackmanharris
 
 
 class AudioVisualizer:
-    def __init__(self, PyAudio, format=pyaudio.paInt16, channels=1, sample_rate=48000, chunk_size=1024, log_mode=False,
-                 decay_speed=0.5):
+    """
+    Takes a py_audio instance to create an audio stream
+    and plot the audio's waveform and frequency spectrum
+    """
+
+    def __init__(self, py_audio, data_format=pyaudio.paInt16,
+                 channels=1, sample_rate=48000, chunk_size=1024,
+                 log_mode=False, decay_speed=0.5):
         # setting object variables
-        self.PyAudio = PyAudio
-        self.format = format
+        self.py_audio = py_audio
+        self.data_format = data_format
         self.channels = channels
         self.sample_rate = sample_rate
         self.chunk_size = chunk_size
@@ -33,7 +42,7 @@ class AudioVisualizer:
         self.min_freq = 0
 
         # x-axis data for plotting
-        self.x = np.arange(0, 2 * self.chunk_size)
+        self.x_wav = np.arange(0, 2 * self.chunk_size)
 
         # log mode does not support step mode, so x-axis must be shortened
         if self.log_mode:
@@ -66,10 +75,23 @@ class AudioVisualizer:
         # additional variables
         self.frames = []
         self.queue = Queue(-1)
+        self.event = Event()
         self.prev_y_fft = None
-        self.prev_y = None
+        self.prev_y_wav = None
 
     def set_plotdata(self, name, data_x, data_y):
+        """
+        Overwrites data on specified plot with new data
+
+        :param name: The name of the plot to be written to
+        :type name: str
+
+        :param data_x: The x component of the data
+        :type data_x: numpy array
+
+        :param data_y: The y component of the data
+        :type data_y: numpy array
+        """
 
         # runs if data has been plotted before
         if name in self.traces:
@@ -107,7 +129,7 @@ class AudioVisualizer:
                         20, self.max_freq, padding=0.005)
 
     # ripped and edited from Stack Overflow
-    def decode(self, in_data, channels, format=np.int16):
+    def decode(self, in_data, channels, data_format=np.int16):
         """
         Convert a byte stream into a 2D numpy array with
         shape (chunk_size, channels)
@@ -120,8 +142,8 @@ class AudioVisualizer:
         if channels > 2:
             print("Only up to 2 channels supported!")
 
-        # read data from buffer as specified format (we default to paInt16 so the stream should be read as int16)
-        result = np.frombuffer(in_data, dtype=format)
+        # read data from buffer as specified format (default int16)
+        result = np.frombuffer(in_data, dtype=data_format)
 
         # calculate length of data after splitting into channels
         chunk_length = len(result) / channels
@@ -133,22 +155,27 @@ class AudioVisualizer:
         return result
 
     def update(self):
+        """
+        Retrieve and process data from the queue to update the
+        PyQt plots
+        """
+
         # get and pre-process data
         data = self.queue.get()
-        self.queue.queue.clear()
+        self.queue.queue.clear()    # clears queue to ensure always grabbing newer data
         data = self.decode(data, self.channels, np.int16)
 
         # calculate waveform of data
-        y = np.mean(data, axis=1)
-        y = y / self.max_freq
-        y = (y + 1) / 2
+        y_wav = np.mean(data, axis=1)
+        y_wav = y_wav / self.max_freq   # shifts y_wav to [-1, 1]
+        y_wav = (y_wav + 1) / 2         # shifts y_wav to [0, 1]
 
         # smooths waveform values to be more easy on the eyes
-        if self.prev_y is not None:
-            y = (1 - self.decay_speed) * self.prev_y + self.decay_speed * y
+        if self.prev_y_wav is not None:
+            y_wav = (1 - self.decay_speed) * self.prev_y_wav + self.decay_speed * y_wav
 
-        # new variable to store any non-essential y transformations
-        new_y = np.concatenate((y, np.flipud(y)))
+        # new variable to store any non-essential y_wav transformations
+        new_y = np.concatenate((y_wav, np.flipud(y_wav)))
 
         # apply blackman harris window to data
         window = blackmanharris(self.chunk_size)
@@ -157,28 +184,33 @@ class AudioVisualizer:
         # calculate fourier transform of data
         y_fft = np.abs(np.fft.rfft(data, n=self.fft_size * 2))
         y_fft = np.delete(y_fft, len(y_fft) - 1)
-        y_fft = y_fft * 2 / (self.max_freq * 256)
-        y_fft = y_fft ** 0.5
+        y_fft = y_fft * 2 / (self.max_freq * 256)   # shifts y_fft to [0, 1]
+        y_fft = y_fft ** 0.5                        # emphasize smaller peaks
 
         # smooths frequency spectrum values to be more easy on the eyes
         if self.prev_y_fft is not None:
             y_fft = (1 - self.decay_speed) * self.prev_y_fft + self.decay_speed * y_fft
 
         # plot all data
-        self.set_plotdata('waveform', self.x, new_y)
+        self.set_plotdata('waveform', self.x_wav, new_y)
         self.set_plotdata('spectrum', self.x_fft, y_fft)
 
         # previous value updates
-        self.prev_y = y
+        self.prev_y_wav = y_wav
         self.prev_y_fft = y_fft
 
     def open_stream(self):
+        """
+        Opens a stream from the py_audio instance and
+        continuously puts data into a queue
+        """
+
         # creates stream to computer's audio devices, aka audio I/O
-        stream = self.PyAudio.open(format=self.format,
-                                   channels=self.channels,
-                                   rate=self.sample_rate,
-                                   input=True,
-                                   frames_per_buffer=self.chunk_size, )
+        stream = self.py_audio.open(format=self.data_format,
+                                    channels=self.channels,
+                                    rate=self.sample_rate,
+                                    input=True,
+                                    frames_per_buffer=self.chunk_size,)
 
         # reads from stream and adds to queue until stopped
         while not self.event.is_set():
@@ -188,18 +220,21 @@ class AudioVisualizer:
 
         # closes all streams
         stream.close()
-        self.PyAudio.terminate()
+        self.py_audio.terminate()
 
     def start(self):
+        """
+        Executes the application and stream I/O thread
+        """
+
         # creates PyQt timer to automatically update the graph every 20ms
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
         timer.start(20)
 
         # sets up stream thread
-        self.event = Event()
-        self.thread = Thread(target=self.open_stream)
-        self.thread.start()
+        thread = Thread(target=self.open_stream)
+        thread.start()
 
         # runs the application if not already running
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
@@ -209,9 +244,9 @@ class AudioVisualizer:
         self.event.set()
 
         # writes recorded audio to file
-        """wf = wave.open("louden.wav", 'wb')
-        wf.setnchannels(self.channels)
-        wf.setsampwidth(self.PyAudio.get_sample_size(self.format))
-        wf.setframerate(self.sample_rate)
-        wf.writeframes(b''.join(self.frames))
-        wf.close()"""
+        #wf = wave.open("louden.wav", 'wb')
+        #wf.setnchannels(self.channels)
+        #wf.setsampwidth(self.py_audio.get_sample_size(self.format))
+        #wf.setframerate(self.sample_rate)
+        #wf.writeframes(b''.join(self.frames))
+        #wf.close()
